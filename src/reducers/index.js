24bafc3b9b2a87b1fb7e24eb24113abedb010280
Camila Montes Celinski <camicelinski@gmail.com>
2022-10-28 22:33:45 +0200

    Install json-server, redux and set store

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const initState = {
+  meetings: []
+}
+
+const reducer = (state = initState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
